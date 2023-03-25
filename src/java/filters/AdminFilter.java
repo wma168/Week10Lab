c2fdd7569707ea5dfccc6481fd8232af0734d9b9
Wilma A. <122852830+wma168@users.noreply.github.com>
2023-03-24 18:51:32 -0600

    Added Admin NetBeans project - Authentication

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,60 @@
+package filters;
+
+import dataaccess.UserDB;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+/**
+ *
+ * @author xbali
+ */
+public class AdminFilter implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+            // code that is executed before the servlet
+            HttpServletRequest httpRequest = (HttpServletRequest)request;
+            HttpSession session = httpRequest.getSession();
+            String email = (String)session.getAttribute("email");
+            
+            UserDB userDb = new UserDB();
+            User user = userDb.get(email);
+            
+            if (user.getRole().getRoleId() == 1) {
+                chain.doFilter(request, response);
+                return;
+            }           
+            
+            else {
+                HttpServletResponse httpResponse = (HttpServletResponse)response;
+                httpResponse.sendRedirect("login");
+            }
+            
+            
+            // code that is executed after the servlet
+            
+        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+       
+    }
+}
